951db9327fd03b8475d4910f4edc3a0feb0b010d
Jonathan Lin <jonathanlin.m@gmail.com>
2014-05-22 19:03:31 -0700

    merged in local activity progress

---
+++ HelloFacebookSample/src/com/facebook/samples/hellofacebook/Challenge.java
@@ -0,0 +1,75 @@
+package com.facebook.samples.hellofacebook;
+
+public class Challenge {
+	
+	private int id;
+	private String title;
+	private String description;
+	private String createdAt;
+	private String updatedAt;
+	private String city;
+	private String state;
+	private String country;
+
+	
+	public Challenge(int id, String title, String description) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(String createdAt) {
+		this.createdAt = createdAt;
+	}
+	public String getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(String updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	
+
+
+}
